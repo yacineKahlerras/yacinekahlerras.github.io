8913eab314525db198a25429bda7fd7d429cfcd2
yacine <kahlerasse@gmail.com>
2025-07-19 12:19:46 +0100

    updating commands

--- next-app/next.config.ts
+++ next-app/next.config.ts
@@ -1,9 +1,6 @@
-const isGithubPages = process.env.NODE_ENV === "production";
-const repo = "yacinekahlerras.github.io"; // your repo name
-
 module.exports = {
   output: "export",
-  basePath: isGithubPages ? `/${repo}` : "",
-  assetPrefix: isGithubPages ? `/${repo}/` : "",
+  basePath: "",
+  assetPrefix: "",
   trailingSlash: true,
 };
