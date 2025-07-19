b5c08f49fcc4c5c4abc6d5ee0e6089589a03b688
yacine <kahlerasse@gmail.com>
2025-07-19 12:09:36 +0100

    fix for config

--- next-app/next.config.ts
+++ next-app/next.config.ts
@@ -1,4 +1,9 @@
+const isGithubPages = process.env.NODE_ENV === "production";
+const repo = "yacinekahlerras.github.io"; // your repo name
+
 module.exports = {
   output: "export",
+  basePath: isGithubPages ? `/${repo}` : "",
+  assetPrefix: isGithubPages ? `/${repo}/` : "",
   trailingSlash: true,
 };
