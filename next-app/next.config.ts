708b9cfad639f753fea40de54389d67a4d11f244
yacine <kahlerasse@gmail.com>
2025-07-19 11:02:05 +0100

    feat:making a static site

--- next-app/next.config.ts
+++ next-app/next.config.ts
@@ -2,7 +2,8 @@ const isGithubPages = process.env.NODE_ENV === "production";
 const repo = "yacinekahlerras.github.io";
 
 module.exports = {
-  assetPrefix: isGithubPages ? `/${repo}/` : "",
+  output: "export",
   basePath: isGithubPages ? `/${repo}` : "",
+  assetPrefix: isGithubPages ? `/${repo}/` : "",
   trailingSlash: true,
 };
